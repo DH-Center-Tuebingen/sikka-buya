e98e854fec522f8830361e531fca3b478fccfa8d
Sev <captain.sev@outlook.com>
2023-06-15 20:10:32 +0200

    asd

--- frontend/src/utils/Setup.js
+++ frontend/src/utils/Setup.js
@@ -3,5 +3,5 @@ import Query from '../database/query';
 export async function superUserIsSet() {
     // const result = await Query.raw(`{isSuperUserSet}`)
     // return result.data.data.isSuperUserSet
-    return false
+    return true
 }
