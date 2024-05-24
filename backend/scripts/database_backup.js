2b4a2294c3edd2b4b570b728076b1168e3611529
Sev <captain.sev@outlook.com>
2024-05-24 11:33:14 +0200

    Updated DB backup file:
    + Added manual
    + Options can be set in .env with 'BACKUP_' prefix

--- backend/scripts/database_backup.js
+++ backend/scripts/database_backup.js
@@ -1,3 +1,64 @@
+const ENV_PREFIX = "BACKUP_"
+
+
+function include(arr) {
+    return (value) => {
+        value.toLowerCase()
+        return arr.includes(value)
+    }
+}
+
+function boolean() {
+    return (value) => include(["true", "false"])(value)
+}
+
+function isAnyString() {
+    return (value) => typeof value === "string"
+}
+
+const validateOptions = {
+    mode: include(["data", "schema", "both", "backend-schema"]),
+    owner: boolean(),
+    format: include(["custom", "text"]),
+    inserts: boolean(),
+    out: isAnyString(),
+    outAbsolute: isAnyString(),
+    name: isAnyString()
+}
+
+function showManual() {
+    console.log(`
+    Usage: node database_backup.js [options]
+
+    Creates a backup of the database based on the configured .env file.
+    This script must be run in the 'backend' directory, as it loads the .env file from there.
+
+    The following options can be passed in the command line or be defined inside the .env file.
+    Inside the .env file, the options must be prefixed with '${ENV_PREFIX}' e.g. BACKUP_OUT_ABSOLUTE=/path/to/backup
+
+
+    Options:
+
+    [no option | ? | --help | -h]   Shows this manual. 
+    [--mode=?]                      Backs up the database in the specified mode:
+                                    --mode=data: Data only
+                                    --mode=schema: Schema only
+                                    --mode=both: Both schema and data *Default*
+                                    --mode=backend-schema: Updates the schema file located at backend/schema.sql
+
+    [--owner=?]                     If set to true, the owner of the database objects will be included in the dump. Default is false.
+    [--format=?]                    The format of the dump file:
+                                    --format=custom: Custom format *Default*
+                                    --format=text: Plain text format
+
+    [--inserts=?]                   If set to true, the dump will contain INSERT commands to restore the data. Default is false.
+    [--out=?]                       The directory where the dump file will be saved, relative to the 'backend' directory. Default is the current directory. 
+                                    This will override --outAbsolute, if both are present.
+
+    [--outAbsolute=?]               The absolute path where the dump file will be saved. Default is the current directory.
+    [--name=?]                      The name of the dump file. Default is the database name followed by the mode and the current date and time.
+    `)
+}
 /**
  * Creates a backup of the database
  * based on the configured .env file.
@@ -41,13 +102,12 @@ function zeroPad(val) {
 function constructFileName(prefix, ext) {
     let date = new Date()
     const name = `${prefix}_${date.getUTCFullYear()}${zeroPad(date.getUTCMonth() + 1)}${zeroPad(date.getUTCDate())}T${zeroPad(date.getUTCHours())}${zeroPad(date.getUTCMinutes())}${zeroPad(date.getUTCSeconds())}.${ext}`
-    return path.join(os.homedir(), name)
+    return path.join(name)
 }
 
 function dump(fileName, options) {
     return new Promise((resolve, reject) => {
-        const backupPath = os.homedir() + "/backup"
-        const command = `pg_dump --username ${process.env.DB_USER} --no-privileges --exclude-table-data app_user --no-password ${options}  --file ${backupPath}/${fileName} ${process.env.DB_NAME} `
+        const command = `pg_dump --username ${process.env.DB_USER} --no-privileges --exclude-table-data app_user --no-password ${options}  --file ${fileName} ${process.env.DB_NAME} `
         execute(command).then(() => {
             console.log(`Successfully exported dump to: ${fileName}`)
             resolve()
@@ -59,22 +119,32 @@ function dump(fileName, options) {
 }
 
 let args = process.argv.slice(2)
-const options = {
+
+if (args.length <= 1) {
+    const noArgs = args.length === 0
+    const isHelp = args.length === 1 && (args[0] === "--help" || args[0] === "-h" || args[0] === "?")
+    if (noArgs || isHelp) {
+        showManual()
+        process.exit(0)
+    }
+}
+
+const defaultOptions = {
     mode: "both",
     owner: false,
     format: "custom",
     inserts: false,
     out: null,
+    outAbsolute: null,
     name: null,
 }
 
-args = args.forEach(element => {
-    let [key, value] = element.split("=")
-    if (key && value) {
-        key = key.replace("--", "")
-        options[key] = value
-    } else throw new Error(`Argument needs a value: ${element}`)
-});
+let loadedOptions = {}
+loadedOptions = loadOptionsFromEnvFile(loadedOptions)
+loadedOptions = loadOptionsFromCommandLineArguments(args, loadedOptions)
+
+
+let options = Object.assign({}, defaultOptions, loadedOptions)
 
 let additionalOptions = []
 
@@ -121,15 +191,72 @@ if (options.mode == "backend-schema") {
 
     const fileName = (options.name) ? options.name : constructFileName(`${process.env.DB_NAME}_${name}`, ext)
     let outputFile = fileName
+
     if (options.out) {
         let outDir = join(process.cwd(), options.out)
         mkdirSync(outDir, { recursive: true })
         outputFile = join(outDir, fileName)
+    } else if (options.outAbsolute) {
+        let outputPath = options.outAbsolute
+        mkdirSync(outputPath, { recursive: true })
+        outputFile = join(outputPath, fileName)
     }
-
+    
     dump(outputFile, additionalOptions.join(" ")).finally(process.exit)
 }
 
+function loadOptionsFromEnvFile(loadedOptions) {
+    for (let key in process.env) {
+        if (key.startsWith(ENV_PREFIX)) {
+            let option = key.replace(ENV_PREFIX, "")
+            let optionKey = option.split("_").reduce((acc, val) => {
+                val = val.toLowerCase()
+                if (acc === "") return val
+                else {
+                    return acc + val.charAt(0).toUpperCase() + val.slice(1)
+                }
+            }, "")
+            const value = process.env[key].trim()
+            checkForConflics(loadedOptions, optionKey, value)
+            loadedOptions[optionKey] = value
+        }
+    }
+    return loadedOptions
+}
 
+function loadOptionsFromCommandLineArguments(args, loadedOptions) {
+    args = args.forEach(element => {
+        let [key, value] = element.split("=")
 
+        if (key && value) {
+            key = key.replace("--", "")
+            value = value.trim()
+            checkForConflics(loadedOptions, key, value)
+            loadedOptions[key] = value
+        } else throw new Error(`Argument needs a value: ${element}`)
+    });
 
+    return loadedOptions
+}
+
+function checkForConflics(options, key, value) {
+    if (!defaultOptions.hasOwnProperty(key)) {
+        throwUnknownOptionError(key)
+    }
+
+    if (options.hasOwnProperty(key)) {
+        throwDuplicateOptionError(key)
+    }
+
+    if (!validateOptions[key](value)) {
+        throw new Error(`Invalid value for ${key}: ${value}`)
+    }
+}
+
+function throwUnknownOptionError(option) {
+    throw new Error(`Unknown option: ${option}`)
+}
+
+function throwDuplicateOptionError(option) {
+    throw new Error(`Duplicate option: ${option}`)
+}
