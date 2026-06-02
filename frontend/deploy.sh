ed3c6b7ad7ead217762b7435985d2f190377519f
severino <severin.opel@outlook.com>
2026-06-02 13:42:17 +0200

    Modified deploy script

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -19,19 +19,25 @@ trap 'log_error "$?" "$LINENO" "$BASH_COMMAND"' ERR
 
 echo "This script will:\n1. Build the vue app.\n2. Remove existing project in 'www/html'.\n3. Copy new project to 'www/html'."
 
-echo "[1/5] Build frontend vue app"
+echo "[1/6] Build frontend vue app"
 npm run build
 
-echo "[2/5] Remove last backup"
+echo "[2/6] Remove last backup"
 sudo rm -rf /var/www/html_backup/*
 
-echo "[3/5] Move old version to backup folder"
+echo "[3/6] Move old version to backup folder"
 sudo mv /var/www/html/* /var/www/html_backup
 
-echo "[4/5] Move new version to webroot"
+echo "[4/6] Move new version to webroot"
 sudo mv ~/sikka-buya/frontend/dist/* /var/www/html
 
-echo "[5/5] Copy cms data from backup to production folder"
-sudo cp -r /var/www/html_backup/data/cms/ /var/www/html/data
+echo "[5/6] Copy cms data from backup to production folder"
+sudo mkdir -p /var/www/html/data/cms
+sudo cp -a /var/www/html_backup/data/cms/. /var/www/html/data/cms/
+
+owner_user="${SUDO_USER:-$USER}"
+owner_group="$(id -gn "$owner_user")"
+echo "[6/6] Set cms ownership to ${owner_user}:${owner_group}"
+sudo chown -R "${owner_user}:${owner_group}" /var/www/html/
 
 echo "Done! Thanks for deploying a new version of sikka:buya"
