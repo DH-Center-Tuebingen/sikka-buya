1066af6fc2ad340f0c932291966c81b87c3b6cfb
severino <severin.opel@outlook.com>
2026-06-15 12:55:17 +0200

    Import treasure color adjusted

--- backend/src/resolver/_ottoman/import_resolver.js
+++ backend/src/resolver/_ottoman/import_resolver.js
@@ -191,7 +191,7 @@ async function ensureTreasure(treasure, cachedTreasures) {
             cachedTreasures[treasure.name] = fetchedTreasure
             existingTreasure = fetchedTreasure
         } else {
-            treasure.color = '#000000'
+            treasure.color = treasure.singleFind ? '#000000' : '#ff0000';
             const id = await Treasure.add(treasure);
             treasure.id = id;
             cachedTreasures[treasure.name] = treasure
